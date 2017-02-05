285a6fbd4d57d2b0eba9ace21b89a0033208e7cd
Ian Martiny <ian.martiny@colorado.edu>
2017-02-05 13:24:24 -0700

    update for rotation

--- app/src/main/java/com/example/servicetimer/MainActivity.java
+++ app/src/main/java/com/example/servicetimer/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.Ringtone;
@@ -25,6 +26,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
     private Button startButton;
     private Button pauseButton;
@@ -33,18 +36,16 @@ public class MainActivity extends AppCompatActivity {
     private TextView timerValue;
     private TextView timerValueMils;
 
-    private long miliTime;
+    private long miliTime = 0L;
 
     private int beepTime = 30;
 
     private boolean running = false;
-    private boolean beep = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        miliTime = 0L;
         timerValue = (TextView) findViewById(R.id.timerValue);
         timerValueMils = (TextView) findViewById(R.id.timerValueMils);
 
@@ -52,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
 
         startButton = (Button) findViewById(R.id.startButton);
 
-        startButton.setOnClickListener(new View.OnClickListener() {
+        /*startButton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
                 if (running){
@@ -65,11 +66,11 @@ public class MainActivity extends AppCompatActivity {
                 running = true;
                 resetButton.setVisibility(View.GONE);
             }
-        });
+        });*/
 
         pauseButton = (Button) findViewById(R.id.pauseButton);
 
-        pauseButton.setOnClickListener(new View.OnClickListener() {
+        /*pauseButton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
                 if(!running){
@@ -79,11 +80,11 @@ public class MainActivity extends AppCompatActivity {
                 stopService(new Intent(MainActivity.this, TimerService.class));
                 resetButton.setVisibility(View.VISIBLE);
             }
-        });
+        });*/
 
         resetButton = (Button) findViewById(R.id.resetButton);
 
-        resetButton.setOnClickListener(new View.OnClickListener() {
+        /*resetButton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
                 stopService(new Intent(MainActivity.this, TimerService.class));
@@ -93,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
                 ((TextView) findViewById(R.id.timerValueMils)).setText(R.string.timerValMils);
                 beep = false;
             }
-        });
+        });*/
 
         Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter
@@ -120,6 +121,35 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void startButton(View view){
+        if (running){
+            return;
+        }
+        Intent i = new Intent(MainActivity.this,TimerService.class);
+        i.putExtra("timer",miliTime);
+
+        startService(i);
+        running = true;
+        resetButton.setVisibility(View.GONE);
+    }
+
+    public void pauseButton(View view){
+        if(!running){
+            return;
+        }
+        running = false;
+        stopService(new Intent(MainActivity.this, TimerService.class));
+        resetButton.setVisibility(View.VISIBLE);
+    }
+
+    public void resetButton(View view){
+        stopService(new Intent(MainActivity.this, TimerService.class));
+        running = false;
+        miliTime = 0L;
+        timerValue.setText(R.string.timerVal);
+        timerValueMils.setText(R.string.timerValMils);
+    }
+
     private BroadcastReceiver uiUpdated = new BroadcastReceiver() {
 
         @Override
@@ -130,10 +160,8 @@ public class MainActivity extends AppCompatActivity {
                 long secs = miliTime/1000;
                 int mins = (int) (secs/60);
                 secs = secs % 60;
-                if (secs > 0)
-                    beep = true;
 
-                if ((miliTime % (beepTime*1000) == 0) && beep)
+                if (miliTime % (beepTime*1000) == 0)
                     beep();
                 int millis = (int) (miliTime % 1000);
 
@@ -158,4 +186,105 @@ public class MainActivity extends AppCompatActivity {
 
         }
     };
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        unregisterReceiver(uiUpdated);
+        stopService(new Intent(MainActivity.this, TimerService.class));
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        /*Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();*/
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            setContentView(R.layout.activity_land);
+            timerValue = (TextView) findViewById(R.id.timerValue_land);
+            timerValueMils = (TextView) findViewById(R.id.timerValueMils_land);
+            long secs = miliTime/1000;
+            int mins = (int) (secs/60);
+            secs = secs % 60;
+            timerValue.setText("" + mins + "  "
+                    + String.format("%02d", secs));
+            int millis = (int) (miliTime % 1000);
+            timerValueMils.setText(String.format("%02d", millis/10));
+            startButton = (Button) findViewById(R.id.startButton_land);
+            pauseButton = (Button) findViewById(R.id.pauseButton_land);
+            resetButton = (Button) findViewById(R.id.resetButton_land);
+            if ((!running) && miliTime > 0)
+                resetButton.setVisibility(View.VISIBLE);
+
+            Spinner dropdown = (Spinner)findViewById(R.id.spinner1_land);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter
+                    .createFromResource(this, R.array.times,
+                            android.R.layout.simple_spinner_item);
+            dropdown.setAdapter(adapter);
+            //dropdown.setSelection(1);
+            int index = Arrays.asList(getResources().getStringArray(R.array.times)).indexOf(Integer.toString(beepTime));
+            dropdown.setSelection(index);
+
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view,
+                                           int position, long id) {
+                    // On selecting a spinner item
+                    String label = parent.getItemAtPosition(position).toString();
+                    beepTime = Integer.parseInt(label);
+                }
+
+                public void onNothingSelected(AdapterView<?> parent) {}
+            });
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            setContentView(R.layout.activity_main);
+            timerValue = (TextView) findViewById(R.id.timerValue);
+            timerValueMils = (TextView) findViewById(R.id.timerValueMils);
+            long secs = miliTime/1000;
+            int mins = (int) (secs/60);
+            secs = secs % 60;
+            timerValue.setText("" + mins + "  "
+                    + String.format("%02d", secs));
+            int millis = (int) (miliTime % 1000);
+            timerValueMils.setText(String.format("%02d", millis/10));
+            startButton = (Button) findViewById(R.id.startButton);
+            pauseButton = (Button) findViewById(R.id.pauseButton);
+            resetButton = (Button) findViewById(R.id.resetButton);
+            if ((!running) && miliTime > 0)
+                resetButton.setVisibility(View.VISIBLE);
+
+            Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter
+                    .createFromResource(this, R.array.times,
+                            android.R.layout.simple_spinner_item);
+            dropdown.setAdapter(adapter);
+            //dropdown.setSelection(1);
+            int index = Arrays.asList(getResources().getStringArray(R.array.times)).indexOf(Integer.toString(beepTime));
+            dropdown.setSelection(index);
+
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view,
+                                           int position, long id) {
+                    // On selecting a spinner item
+                    String label = parent.getItemAtPosition(position).toString();
+                    beepTime = Integer.parseInt(label);
+                }
+
+                public void onNothingSelected(AdapterView<?> parent) {}
+            });
+        }
+    }
 }
